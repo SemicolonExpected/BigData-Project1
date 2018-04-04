ea8513e51588571f6bacfc895e50410b9f128b97
Victoria Zhong <SemicolonExpected@users.noreply.github.com>
2018-04-04 18:04:17 -0400

    Update queryADKB.php

--- RelationalDB/queryADKB.php
+++ RelationalDB/queryADKB.php
@@ -3,7 +3,7 @@
 $db = mysql_connect('localhost', 'bigdata', 'bigdata');
 mysql_select_db("ADKnowledgeBase", $db);
 
-$query = "'".$_GET['query']."'";
+$query = $_GET['query'];
 
 $querytype = $_GET['type'];
 
@@ -12,8 +12,10 @@ echo $_GET['type'].";".$_GET['query'];
 if($querytype == "getInteractingGenes"){
 
 	$getGenes = 'SELECT b FROM EntrezGeneSymbols INNER JOIN PPI ON EntrezGeneSymbols.entrez_id = PPI.a WHERE a = "'.$query.'";';
-	//echo "hello";
-	//$getGenes = 'SELECT b FROM PPI WHERE b = "'17'";';
+	//echo ";hello";
+	//echo ";goodbye";
+	//echo ";". $getGenes;
+	//$getGenes = 'SELECT b FROM PPI WHERE a = "17";';
 	$rows = mysql_query($getGenes);
 
 	while($rowData = mysql_fetch_row($rows)){
@@ -28,6 +30,8 @@ if($querytype == "getInteractingGenes"){
 
 if($querytype == "getPatientData"){
 	$getPatient = 'SELECT age, gender, education FROM Patients WHERE patient_id = "'.$query.'";';
+	//$getPatient = 'SELECT age, gender, education FROM Patients WHERE patient_id = "X764_130520";';
+
 	$rows = mysql_query($getPatient);
 
 	$rowData = mysql_fetch_row($rows);
